148f825973e0fcf58f1ad41b3786c30f43e45655
ITrunner1 <MatveyT1337@yandex.ru>
2024-05-18 19:09:16 +1000

    Installed dependencies, configured configs

---
+++ src/lib/format-number-to-k.ts
@@ -0,0 +1,15 @@
+function formatNumberToK(num: number) {
+    if(num >= 1000000000) 
+        return (num/1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
+    
+    if(num >= 10000000) 
+        return (num/10000000).toFixed(1).replace(/\.0$/, '') + ' M';
+    
+    if(num >= 100000) 
+        return (num/100000).toFixed(1).replace(/\.0$/, '') + ' Lac';
+
+    if(num >= 1000) 
+        return (num/1000).toFixed(1).replace(/\.0$/, '') + 'K';
+    
+    return num;
+}
