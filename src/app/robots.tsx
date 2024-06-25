52868a08bae5158251f25b34c1817f637fa1f8ec
ITrunner1 <MatveyT1337@yandex.ru>
2024-06-26 04:12:48 +1000

    Edit robots

--- src/app/robots.tsx
+++ src/app/robots.tsx
@@ -3,8 +3,7 @@ import { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
+      userAgent: 'Yandex',      
       disallow: ['/admin', 'privacy'],
     },
     sitemap: 'https://red-music.fun/sitemap.xml',
